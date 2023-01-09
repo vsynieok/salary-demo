b2d3339f4ad3956c4de522dd8ce8450ca6972b6d
Vladyslav Synieok <55637550+vsynieok@users.noreply.github.com>
2023-01-10 00:03:05 +0200

    10.01

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,13 +1,62 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Radio } from "antd";
+import { useForm } from "antd/es/form/Form";
+import { useState } from "react";
 import { useAppDispatch } from "../../app/hooks";
 import { addMessage } from "../Logger/LoggerSlice";
 
 const Calculator: React.FC = () => {
+  const [form] = useForm();
+
   const dispatch = useAppDispatch();
+  const [withTax, setWithTax] = useState<number>(0);
+  const [taxableIncome, setTaxableIncome] = useState<number>(0);
+  const [incomeTax, setIncomeTax] = useState<number>(0);
+
+  const [monthlyIncome, setMonthlyIncome] = useState<number>(0);
+
+  const calculateTax = (
+    taxableIncome: number,
+    monthlyIncome: number,
+    taxOption: number
+  ) => {
+    console.log(taxOption);
+    let tax;
+    switch (taxOption) {
+      case 0:
+        tax = 0.2 * (taxableIncome - 654);
+        return tax;
+      case 1:
+        let exemption = 654 - 0.72667 * (monthlyIncome - 1200);
+        tax = 0.2 * (taxableIncome - exemption);
+        return tax;
+      case 2:
+        tax = 0.2 * taxableIncome;
+        return tax;
+      default:
+        return 0;
+    }
+  };
 
   const onValuesChange = (c: any, v: any) => {
-    console.log(c);
-    console.log(v);
+    let gross = parseInt(v.gross);
+    let monthlyIncome = parseInt(v.monthlyIncome);
+
+    if (monthlyIncome <= 1200) {
+      form.setFieldsValue({ exemption: 0 });
+    } else if (monthlyIncome > 1200 && monthlyIncome < 2100) {
+      form.setFieldsValue({ exemption: 1 });
+    } else {
+      form.setFieldsValue({ exemption: 2 });
+    }
+
+    setMonthlyIncome(monthlyIncome);
+
+    let taxable = gross - (v.pension === 1 ? gross * 0.02 : 0) - gross * 0.016;
+    setTaxableIncome(taxable);
+
+    let incomeTax = calculateTax(taxable, v.monthlyIncome, v.exemption);
+    setIncomeTax(incomeTax);
+    setWithTax(gross + incomeTax + gross * 0.33 + gross * 0.08);
   };
 
   const onClick = () => {
@@ -16,18 +65,35 @@ const Calculator: React.FC = () => {
 
   return (
     <div style={{ width: 400 }}>
-      <Form onValuesChange={onValuesChange}>
-        <Form.Item label="Salary" name="salary">
+      <Form form={form} onValuesChange={onValuesChange}>
+        <Form.Item label="Gross" name="gross">
           <Input></Input>
         </Form.Item>
-        <Form.Item label="Salary2" name="salary2">
+        <Form.Item label="Monthly income" name="monthlyIncome">
           <Input></Input>
+        </Form.Item>
+        <Form.Item label="Basic exemption" name="exemption" initialValue={0}>
+          <Radio.Group>
+            <Radio value={0}>{"654 euros"}</Radio>
+            <Radio value={1}>{"Based on income"}</Radio>
+            <Radio value={2}>{"No exemption"}</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="Pension" name="pension" initialValue={0}>
+          <Radio.Group>
+            <Radio value={0}>{"0%"}</Radio>
+            <Radio value={1}>{"2%"}</Radio>
+          </Radio.Group>
         </Form.Item>
         <Form.Item>
           <Button type="primary" htmlType="submit" onClick={onClick}>
-            Pay salary
+            Issue an invoice
           </Button>
         </Form.Item>
+        <Form.Item label="Taxable income">{taxableIncome}</Form.Item>
+        <Form.Item label="Income tax">{incomeTax}</Form.Item>
+        <Form.Item label="Social tax (33%)">{withTax * 0.33}</Form.Item>
+        <Form.Item label="Total cost for employer">{withTax}</Form.Item>
       </Form>
     </div>
   );
--- src/components/Logger/Logger.tsx
+++ src/components/Logger/Logger.tsx
@@ -1,19 +1,12 @@
 import { Input } from "antd";
-import { useEffect, useState } from "react";
-import { store } from "../../app/store";
+import { useAppSelector } from "../../app/hooks";
 
 const Logger: React.FC = () => {
-  const loggerState = store.getState().logger;
-  const [message, setMessage] = useState<string>("");
+  const messages = useAppSelector((state) => state.logger.messages);
 
   const getMessages = () => {
-    let messages = store.getState().logger.messages;
     return messages.join("\n");
   };
-
-  useEffect(() => {
-    setMessage(getMessages());
-  }, [store.getState().logger.messages]);
 
   return (
     <div style={{ width: 400, height: "100%" }}>
@@ -21,7 +14,7 @@ const Logger: React.FC = () => {
         autoSize={{ minRows: 4, maxRows: 10 }}
         rows={4}
         readOnly
-        value={message}
+        value={getMessages()}
       ></Input.TextArea>
     </div>
   );
